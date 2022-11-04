7de8fb31ff22dd8ddcd6b590af1315dcd3e16e85
Audrey Maldonado <audrey@render.com>
2022-11-04 11:37:51 -0400

    upgrade setup tools with pip

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -o errexit
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip 
+
 python3 -m pip install --upgrade pip setuptools wheel
+sudo yum reinstall python34-setuptools
 pip install virtualenv
 virtualenv .
 source /opt/render/project/src/.venv/bin/activate
