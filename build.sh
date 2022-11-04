a84a0b52c71bb97b6cfaa6bc0643dad8fe279af2
Audrey Maldonado <audrey@render.com>
2022-11-04 11:04:19 -0400

    create venv

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip install virtualenv
+virtualenv .
 . venv/bin/activate
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip 
 pip install .
