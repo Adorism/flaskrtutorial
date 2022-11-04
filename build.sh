90029b7f5584c578fdbd3d54a1b8d1eaf95a9ee8
Audrey Maldonado <audrey@render.com>
2022-11-04 11:44:18 -0400

    fussing with setup tools

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip 
 
 python3 -m pip install --upgrade pip setuptools wheel
-reinstall python39-setuptools
+pip reinstall python39-setuptools
 pip install virtualenv
 virtualenv .
 source /opt/render/project/src/.venv/bin/activate
