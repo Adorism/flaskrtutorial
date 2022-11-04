ba4346e1f351fc1a346090dfb2653925cc4a211f
Audrey Maldonado <audrey@render.com>
2022-11-04 12:13:45 -0400

    setup tools line from John

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 set -o errexit
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip 
+
 pip install virtualenv
 virtualenv .
 source /opt/render/project/src/.venv/bin/activate
-
+pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install .
 poetry install 
