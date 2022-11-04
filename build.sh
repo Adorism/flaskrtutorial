63c7cc1c2ce90b7c355a635c491863089f557a7a
Audrey Maldonado <audrey@render.com>
2022-11-04 12:08:19 -0400

    fine, no reinstall

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip 
 
-pip install --reinstall python3-pkg-resources python3-setuptools
+pip install python3-pkg-resources python3-setuptools
 pip install virtualenv
 virtualenv .
 source /opt/render/project/src/.venv/bin/activate
